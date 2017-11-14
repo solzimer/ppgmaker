32c391179dde48fd084d1634a6948275818214b6
solzimer <solzimer@gmail.com>
2017-11-14 18:11:21 +0100

    jquery ui

--- static/js/directives/ppg-draggable.js
+++ static/js/directives/ppg-draggable.js
@@ -1,8 +1,7 @@
 angular.module('ppgmaker').directive("ppgDraggable",function($q) {
 
 	function link(scope,elem,attrs) {
-		var drg = $(elem).draggable();
-
+		var drg = $(elem.get(0)).draggable();
 		scope.$on("$destroy",function(){
 			drg.destroy();
 		});
