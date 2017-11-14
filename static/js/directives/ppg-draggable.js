3aba64297ba2b24fd7461fed036e2181fb793e22
solzimer <solzimer@gmail.com>
2017-11-14 13:38:36 +0100

    jquery ui again

--- static/js/directives/ppg-draggable.js
+++ static/js/directives/ppg-draggable.js
@@ -1,29 +1,11 @@
 angular.module('ppgmaker').directive("ppgDraggable",function($q) {
-	var PPGDR = "ppg-dragging";
-
-	function handleDrag(ev) {
-		if(ev.srcEvent.type=='pointercancel') return;
-
-		var elem = $(ev.target);
-
-		if(!elem.data(PPGDR)) {
-			elem.data(PPGDR,$(elem).position());
-		}
-
-		var pos = elem.data(PPGDR);
-		var posX = ev.deltaX + pos.left;
-		var posY = ev.deltaY + pos.top;
-		$(elem).css({'left':posX,'top':posY});
-
-		if(ev.isFinal) {
-			elem.data(PPGDR,null);
-		}
-	}
 
 	function link(scope,elem,attrs) {
-		var mc = new Hammer(elem[0]);
-		mc.get("pan").set({direction:Hammer.DIRECTION_ALL, threshold:0});
-		mc.on("pan", handleDrag);
+		var drg = $(elem).draggable();
+
+		scope.$on("$destroy",function(){
+			drg.destroy();
+		});
 	}
 
 	return {
