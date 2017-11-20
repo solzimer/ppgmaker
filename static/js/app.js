723db5664fd99dae860842ea72372e421847be4b
solzimer <solzimer@gmail.com>
2017-11-20 18:03:04 +0100

    pouchdb persistence

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,6 @@
 angular.module('ppgmaker', ['ui.bootstrap']).config(function() {
 	console.log("App started!");
 }).controller("MainController",function($scope,$interval,itemsService,sceneService){
-	debugger;
 	var MAX = 5;
 	var MAX_TIME = 30000;
 	var recordTimeout = null;
@@ -11,28 +10,38 @@ angular.module('ppgmaker', ['ui.bootstrap']).config(function() {
 	$scope.play = -1;
 	$scope.time = 0;
 
-	$scope.scene = {
+	$scope.scene = null/*{
 		background : {
 			src : "/img/bg/bg003.jpg"
 		},
 		items : [
 			{src:"/img/bubbles/bubbles001.png",buffer:[]},
 		]
-	}
+	}*/
 
 	function init() {
-		itemsService.get().then(function(items){
+		debugger;
+		itemsService.get().then(items=>{
 			$scope.allitems = items;
 			console.log(items);
 		});
 
-		$scope.$watchCollection("scene.items",function(items){
-			$scope.addDisabled = items.length>=MAX;
+		$scope.$watchCollection("scene.items",items=>{
+			$scope.addDisabled = !items || items.length>=MAX;
+		});
+
+		sceneService.findFilm({name:"testppg"}).then(film=>{
+			if(!film) return sceneService.newFilm({name:"testppg"}).save();
+			else return film;
+		}).then(film=>{
+			$scope.film = film;
+		}).catch(err=>{
+			console.error(err);
 		});
 	}
 
 	function resetBuffers() {
-		$scope.scene.items.forEach(function(item){
+		$scope.scene.items.forEach(item => {
 			item.buffer.splice(0,item.buffer.length);
 		});
 	}
@@ -41,7 +50,7 @@ angular.module('ppgmaker', ['ui.bootstrap']).config(function() {
 		resetBuffers();
 		$scope.time = 0;
 		var ti = Date.now();
-		recordTimeout = $interval(function(){
+		recordTimeout = $interval(()=>{
 			var tn = Date.now() - ti;
 			$scope.time = tn*100/MAX_TIME;
 			if(tn>=MAX_TIME) stopRecord();
@@ -53,9 +62,16 @@ angular.module('ppgmaker', ['ui.bootstrap']).config(function() {
 		$interval.cancel(recordTimeout);
 	}
 
+	$scope.newScene = function() {
+		$scope.film.add({}).then(scene=>{
+			$scope.scene = scene;
+			$scope.$apply();
+		});
+	}
+
 	$scope.addItem = function(item) {
 		if(!$scope.addDisabled)
-			$scope.scene.items.push({src : item.src, buffer : []});
+			$scope.scene.items.push({src:item.src, buffer:[]});
 	}
 
 	$scope.toggleRecord = function() {
--- static/js/services/sceneService.js
+++ static/js/services/sceneService.js
@@ -1,24 +1,105 @@
-angular.module("ppgmaker").service("sceneService",function($q,$http){
+angular.module("ppgmaker").service("sceneService",function(){
 	var self = this;
-	var fdb = new ForerunnerDB();
-	var db = null;
 	var filmCol, sceneCol;
 
 	this.model = {};
 
+	function del(id) {
+		return {_id:id,_deleted:true}
+	}
+
+	class Scene {
+		constructor(props) {
+			props = props || {};
+			this._id = props._id || "scene_"+Date.now();
+			this._rev = props._rev;
+			this.name = props.name || "";
+			this.frames = props.frames || 0;
+			this.background = props.background || "";
+			this.items = props.items || [];
+			this.idfilm = props.idfilm || "";
+			this.screenshot = props.screenshot || "";
+		}
+
+		save() {
+			return sceneCol.put(this).then(res=>this);
+		}
+
+		remove() {
+			filmCol.
+				get(this.idfilm).
+				then(film=>{
+					let idx = film.idscenes.indexOf(this._id);
+					if(idx>=0) {
+						film.idscenes.splice(idx,1);
+						return filmCol.put(film);
+					}
+					else {
+						return film;
+					}
+				}).
+				then(film=>sceneCol.remove(this)).
+				then(res=>this);
+		}
+	}
+
+	class Film {
+		constructor(props) {
+			props = props || {};
+			this._id = props._id || "film_"+Date.now();
+			this._rev = props._rev;
+			this.name = props.name || "";
+			this.idscenes = props.idscenes || [];
+		}
+
+		add(scene) {
+			scene = scene || {};
+			scene.idfilm = this._id;
+			if(!scene._id) scene = new Scene(scene);
+
+			return scene.save().then(scene=>{
+				this.idscenes.push(scene._id);
+				return this.save();
+			}).then(res=>scene);
+		}
+
+		save() {
+			return filmCol.put(this).then(res=>this);
+		}
+
+		remove() {
+			sceneCol.
+				bulkDocs(this.idscenes.map(del)).
+				then(res=>filmCol.remove(this)).
+				then(res=>this);
+		}
+	}
+
 	function init() {
-		db = fdb.db("ppgmaker");
-		filmCol = db.collection("films");
-		sceneCol = db.collection("scenes");
+		filmCol = new PouchDB('ppgmaker_films');
+		sceneCol = new PouchDB('ppgmaker_scenes');
 	}
 
 	this.newFilm = function(film) {
-		return filmCol.newDocument(film);
+		return new Film(film);
+	}
+
+	this.getFilm = function(id) {
+		return filmCol.get(id);
+	}
+
+	this.findFilm = function(query) {
+		return filmCol.
+			createIndex({index: {fields: ['name']}}).
+			then(res => filmCol.find({selector: query})).
+			then(res => res.docs[0]).
+			then(film => film? new Film(film) : null);
 	}
 
 	this.newScene = function(film,scene) {
-		scene.film = film.id;
-		return sceneCol.newDocument(scene);
+		scene = scene || {};
+		scene.idfilm = film.id;
+		return new Scene(scene);
 	}
 
 	init();
