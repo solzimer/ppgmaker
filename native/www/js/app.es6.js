b6ef3057fb1cdd37caeee6cec14206d7514251b1
solzimer <solzimer@gmail.com>
2017-12-27 15:57:11 +0100

    Drag containment

--- native/www/js/app.es6.js
+++ native/www/js/app.es6.js
@@ -270,36 +270,31 @@ angular.module('ppgmaker').directive("ppgCarousel",function($timeout,styleServic
 });
 
 angular.module('ppgmaker').directive("ppgDraggable",function(styleService) {
-	const DRG = "hammer_drag";
-
-	function isOutside(elem,dx,dy) {
-		let box = elem[0].getBoundingClientRect();
-		let ww = window.innerWidth, wh = window.innerHeight;
-		if(box.x<0 && dx<0) return true;
-		else if(box.y<0 && dy<0) return true;
-		else if((box.x+box.width)>ww && dx>0) return true;
-		else if((box.y+box.height)>wh && dy>0) return true;
-		else return false;
-	}
+	const
+		DRG = "hammer_drag",
+		BOX = "hammer_drag_box";
 
 	function handleDrag(ev,scope) {
 		var elem = $(ev.target);
 
 		if (!elem.data(DRG)) {
 			elem.data(DRG,elem.position());
+			elem.data(BOX,elem[0].getBoundingClientRect());
 		}
 
-		if(!isOutside(elem,ev.deltaX,ev.deltaY)) {
-			var oldPos = elem.data(DRG);
-			var posX = ev.deltaX + oldPos.left;
-			var posY = ev.deltaY + oldPos.top;
-			var newPos = {left:posX,top:posY};
-			elem.css(newPos);
-			styleService.set(elem,newPos);
-		}
+		var oldPos = elem.data(DRG);
+		var box = elem.data(BOX);
+		var posX = ev.deltaX + oldPos.left;
+		var posY = ev.deltaY + oldPos.top;
+		posX = Math.min(Math.max(0,posX),window.innerWidth-box.width);
+		posY = Math.min(Math.max(0,posY),window.innerHeight-box.height);
+		var newPos = {left:posX,top:posY};
+		elem.css(newPos);
+		styleService.set(elem,newPos);
 
 		if (ev.isFinal) {
 			elem.data(DRG,null);
+			elem.data(BOX,null);
 			scope.$eval(elem.attr("on-ppg-drop"))(elem.attr("id"));
 			scope.$apply();
 		}
